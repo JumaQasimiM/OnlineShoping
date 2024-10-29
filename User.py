16443b432fc2917f72cddcef34d2e5a4c2e9e8f4
Juma Qasimi <Jumaqasimi@gmail.com>
2024-10-29 23:36:27 +0100

    user class modified!

--- User.py
+++ User.py
@@ -1,3 +1,35 @@
 # User file
 class User:
-	pass
+	""" User class"""
+	roll = 'Admin' # class variable shared by all instances
+	def __init__(self, name, password, roll):
+		# instance variable unique to each instance
+		self.name = name  
+		self.password = password
+		self.roll = roll
+
+	def __repr__(self):
+		return f"user : {self.name}, roll : {self.roll}"
+
+	def get_user_by_id(self, user_id):
+		if user_id == -1:
+			# not found
+			pass
+		else:
+			# form db take the user 
+			pass
+
+	def remove_user(self, user_id):
+		if user_id == -1:
+			# not found
+			pass
+		else:
+			# delete
+			pass
+
+	@staticmethod
+	def get_all_user():
+		pass
+
+
+
