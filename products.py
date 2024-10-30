fcaeedb84a66f9728d7ad5f29be4d1c1f5bea090
Juma Qasimi <Jumaqasimi@gmail.com>
2024-10-30 23:27:02 +0100

    created get_product_by_id in product class

--- products.py
+++ products.py
@@ -14,3 +14,31 @@ class Products:
 	def save(self):
 		pass
 
+	def get_product_by_id(product_id):
+		pass
+
+	def update_stock(product_id, quantity):
+		# update product in stock
+		pass
+
+	def get_all_product():
+		# 'SELECT * FROM products'
+		pass
+
+
+if __name__ == '__main__':
+	new_product = Products(name = 'Computer',description = 'cori-7', 
+							price = 1200,category='Electronice',stock = 144)
+	# save new product
+	new_product.save()
+	print('Product saved successfully!')
+
+	# select a product by id
+	# get_product_by_id() is a static method
+	product_data = Products.get_product_by_id(1)
+
+	if product_data:
+		print('Product details: ', product_data)
+
+	else:
+		print('product not found!')
