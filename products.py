14ffe322d1fbb01facc113ec5117f84aba1e0dcc
Juma Qasimi <Jumaqasimi@gmail.com>
2024-10-30 23:12:25 +0100

    product class created

--- products.py
+++ products.py
@@ -1,4 +1,16 @@
 # Products file
-
+from datetime import datetime
 class Products:
-	pass
+	def __init__(self, name, description, price, category, stock=0):
+		self.name = name
+		self.description = description
+		self.price = price
+		self.category = category
+		self.stock = stock
+		self.created_at = datetime.now()
+
+
+	# save 
+	def save(self):
+		pass
+
