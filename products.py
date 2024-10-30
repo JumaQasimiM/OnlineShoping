b038811316e55e46026300c933fdb230925b34d1
JumaQasimiM <qasimimohammad819@gmail.com>
2024-10-30 23:56:01 +0100

    Update products.py
    import and use databaseconnection

--- products.py
+++ products.py
@@ -1,5 +1,6 @@
 # Products file
 from datetime import datetime
+from databaseconnection import DatabaseConnection
 class Products:
 	def __init__(self, name, description, price, category, stock=0):
 		self.name = name
@@ -13,31 +14,25 @@ class Products:
 	# save 
 	def save(self):
 		# save new product in database
-
 		curser = DatabaseConnection.get_cursor()
-		curser.execute('''INSERT INTO 
-							products (name, description, price, category, stock,created_at) 
-						  VALUES (?,?,?,?,?,?)''',
-						  	(self.name, self.description, self.price, self.category, self.stock))
-
+		curser.execute('''INSERT INTO products (name, description, price, category, stock,created_at) 
+						  VALUES (?,?,?,?,?,?)''',(self.name, self.description, self.price, self.category, self.stock))
 		DatabaseConnection._connection.commit()
 
 
 	def get_product_by_id(product_id):
 		pass
-
 	def update_stock(product_id, quantity):
 		# update product in stock
 		pass
-
-	def get_all_product():
+ 	@saticmethod
+	def get_all_product(cls):
 		# 'SELECT * FROM products'
 		pass
 
 
 if __name__ == '__main__':
-	new_product = Products(name = 'Computer',description = 'cori-7', 
-							price = 1200,category='Electronice',stock = 144)
+	new_product = Products(name = 'Computer',description = 'cori-7', price = 1200,category='Electronice',stock = 144)
 	# save new product
 	new_product.save()
 	print('Product saved successfully!')
@@ -46,8 +41,5 @@ if __name__ == '__main__':
 	# get_product_by_id() is a static method
 	product_data = Products.get_product_by_id(1)
 
-	if product_data:
-		print('Product details: ', product_data)
-
-	else:
-		print('product not found!')
+	if product_data:print('Product details: ', product_data)
+	else:print('product not found!')
