4537502d6cda658a9633d62cbe4c61a1499c2bdb
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-01 00:49:48 +0100

    create save method in category class

--- categories.py
+++ categories.py
@@ -1,5 +1,6 @@
 # Categories file
 from datetime import datetime
+from databaseconnection import DatabaseConnection
 class Category:
 	def __init__(self, name, description, parent_id):
 		self.name  =name
@@ -10,9 +11,14 @@ class Category:
 	def __repr__(self):
 		return f"Category name: {self.name}, description: {self.description}"
 
+	# save into database
+	def save(self):
+		cursor = DatabaseConnection.get_cursor()
+		cursor.execute('''INSERT INTO categories(name, description, parent_id) VALUES('?,?,?')''',
+			(self.name, self.description, self.parent_id))
+		DatabaseConnection._connection.commit()
 
-
-
+	# get by id and get all 
 
 '''
 Parent_id:
@@ -21,7 +27,12 @@ Parent_id:
 3:...
 
 '''
-new category = Category('Book', 'Electronik book',1)
+if __name__ == '__main__':
+	new_category = Category('Book', 'Electronik book',1)
+	# save in database
+	new_category.save()
+	print('Category created successfully!')
+
 
 
 
