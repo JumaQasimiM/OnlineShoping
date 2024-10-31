8aebe8b59f3a0116f1b22ee5affe050cf9dc1190
Juma Qasimi <Jumaqasimi@gmail.com>
2024-10-31 17:51:50 +0100

    add constructor in categorry class

--- categories.py
+++ categories.py
@@ -1,4 +1,17 @@
 # Categories file
-
+from datetime import datetime
 class Category:
-	pass
+	def __init__(self, name, description, parent_id):
+		self.name  =name
+		self.description = description
+		self.parent_id = parent_id
+		self.created_at = datetime.now()
+
+	def __repr__(self):
+		return f"Category name: {self.name}, description: {self.description}"
+
+
+	
+
+
+	
