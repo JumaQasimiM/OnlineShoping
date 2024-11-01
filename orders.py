13677d082c2466c33a3b4f5a047587a6123115d9
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-01 01:24:17 +0100

    create order class

--- orders.py
+++ orders.py
@@ -1,5 +1,48 @@
 # Orders filee
-
+from datetime import datetime
+from databaseconnection import DatabaseConnection
 class Order:
-	pass
-	
+	""" status: pending, completed, cancelled """
+	def __init__(self, user_id, totle_amount, status='pending'):
+		self.user_id = user_id
+		self.totle_amount = totle_amount
+		self.status = status
+		self.created_at = datetime.now()
+
+	# save method
+	def save(self):
+		cursor = DatabaseConnection.get_cursor()
+		cursor.execute('''INSERT INTO orders(user_id, totle_amount, status)
+						  VALUES (?,?,?) ''', (self.user_id, self.totle_amount, self.status))
+		cursor._connection.commit()
+
+	#update stutus
+	@staticmethod
+	def update_status(order_id, new_status):
+		# update the order status
+		cursor = DatabaseConnection.get_cursor()
+		cursor.execute('''UPDATE orders SET status = ? WHERE id =?''',(new_status,order_id))
+		DatabaseConnection._connection.commit()
+
+	# get all user's orders 
+	@staticmethod
+	def get_orders_by_user(user_id):
+		cursor = DatabaseConnection.get_cursor()
+		cursor.execute('SELECT * FROM orders WHERE user_id = ?',(user_id))
+		orders = cursor.fetchall()
+		return orders
+
+
+
+
+if __name__ == '__main__':
+	new_order = Order(user_id = 1, totle_amount = 3,status = 'pending')
+	new1_order = Order(user_id = 1, totle_amount = 4,status = 'completed')
+	new_order.save()
+
+	# update status
+	Order.update_status(1,'completed')
+	print('Order updated successfully!')
+	# get all orders form one user
+	user_orders = Order.get_orders_by_user(1)
+	print('user orders: ', user_orders)
--- reviews.py
+++ reviews.py
@@ -3,14 +3,18 @@ from datetime import datetime
 from databaseconnection import DatabaseConnection
 class Review:
 	def __init__(self, user_id, product_id, rating, comment=None):
-
+		self.user_id = user_id
+		self.product_id = product_id
+		self.rating = rating
+		self.comment = comment
 		self.created_at = datetime.now()
-		pass
+		
 
 	def __repr__(self):
 		return f"user_id = {self.user_id}, product_id = {self.product_id}, rating={self.rating}"
 
 	
+	# save in to database
 	def save(self):
 		cursor = DatabaseConnection.get_cursor()
 		cursor.execute('''INSERT INTO reviews(user_id, product_id, rating, comment)
